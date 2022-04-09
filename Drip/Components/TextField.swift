e30357bdb88a49da3f5a648d8b8b7b36ed8a616f
Mikhail Popov <mikh.popov@vk.team>
2022-04-10 01:01:00 +0300

    fix login view

---
+++ Drip/Components/TextField.swift
@@ -0,0 +1,36 @@
+//
+//  TextField.swift
+//  Drip
+//
+//  Created by mikh.popov on 10.04.2022.
+//
+
+import UIKit
+
+class TextField: UITextField {
+    let padding = UIEdgeInsets(top: 0, left: 12, bottom: 0, right: 12)
+
+    override open func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+
+    override open func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+
+    override open func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+    
+    open func setupDefault(placeholder: String, security: Bool) -> Void {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.attributedPlaceholder = NSAttributedString(
+            string: placeholder,
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.white]
+        )
+        self.textColor = .white
+        self.isSecureTextEntry = security
+        self.layer.cornerRadius = 12
+        self.backgroundColor = UIColor(white: 0, alpha: 0.1)
+    }
+}
--- Drip/Scenes/LoginController.swift
+++ Drip/Scenes/LoginController.swift
@@ -8,49 +8,80 @@
 import UIKit
 
 final class LoginViewController: UIViewController {
+    let logo: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Drip"
+        label.font = UIFont.boldSystemFont(ofSize: 72)
+        label.textColor = .white
+        return label
+    }()
+
     let emailTextField: UITextField = {
-        let tf = UITextField()
-        tf.translatesAutoresizingMaskIntoConstraints = false
-        tf.placeholder = "Email"
-        tf.borderStyle = .roundedRect
-        tf.backgroundColor = UIColor(white: 0, alpha: 0.1)
+        let tf = TextField()
+        tf.setupDefault(placeholder: "Email", security: false)
         tf.addTarget(self, action: #selector(handleTextChange), for: .editingChanged)
         return tf
     }()
     
     let passwordTextField: UITextField = {
-        let tf = UITextField()
-        tf.translatesAutoresizingMaskIntoConstraints = false
-        tf.placeholder = "Password"
-        tf.isSecureTextEntry = true
-        tf.borderStyle = .roundedRect
-        tf.backgroundColor = UIColor(white: 0, alpha: 0.1)
+        let tf = TextField()
+        tf.setupDefault(placeholder: "Password", security: true)
         tf.addTarget(self, action: #selector(handleTextChange), for: .editingChanged)
         return tf
     }()
 
-    let signUpButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("Login", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.layer.cornerRadius = 3
-        button.backgroundColor = UIColor.lightGray
+    let loginButton: UIButton = {
+        let button = Button(type: .system)
+        button.setupDefault(title: "Login", titleColor: .black, backgroundColor: .lightGray)
+        button.addTarget(self, action: #selector(handleLogin), for: .touchUpInside)
+        return button
+    }()
+    
+    let signupButton: UIButton = {
+        let button = Button(type: .system)
+        button.setupDefault(title: "Signup", titleColor: .white, backgroundColor: .black)
         button.addTarget(self, action: #selector(handleLogin), for: .touchUpInside)
         return button
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        setupBackground()
+        setupLogo()
         setupTextFields()
+    }
+    
+    func setupLogo() {
+        view.addSubview(logo)
+        logo.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
+        logo.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40).isActive = true
+        logo.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -40).isActive = true
+        logo.heightAnchor.constraint(equalToConstant: 200).isActive = true
+    }
+
+    func setupBackground() {
+        view.frame = CGRect(x: 0, y: 0, width: 550, height: 844)
+        view.backgroundColor = .white
+        let layer0 = CAGradientLayer()
+        layer0.colors = [
+          UIColor(red: 0.059, green: 0.067, blue: 0.235, alpha: 1).cgColor,
+          UIColor(red: 0.278, green: 0.161, blue: 0.545, alpha: 1).cgColor,
+          UIColor(red: 0.694, green: 0.039, blue: 0.792, alpha: 1).cgColor
+        ]
+        layer0.locations = [0, 0.46, 1]
+        layer0.startPoint = CGPoint(x: 0.25, y: 0.5)
+        layer0.endPoint = CGPoint(x: 0.75, y: 0.5)
+        layer0.transform = CATransform3DMakeAffineTransform(CGAffineTransform(a: -1.11, b: 0.82, c: -1, d: -0.29, tx: 1.61, ty: 0.32))
+        layer0.bounds = view.bounds.insetBy(dx: -0.5*view.bounds.size.width, dy: -0.5*view.bounds.size.height)
+        layer0.position = view.center
+        view.layer.addSublayer(layer0)
     }
 
     func setupTextFields() {
-        let stackView = UIStackView(arrangedSubviews: [emailTextField, passwordTextField, signUpButton])
+        let stackView = UIStackView(arrangedSubviews: [emailTextField, passwordTextField, loginButton, signupButton])
         stackView.axis = .vertical
-        stackView.spacing = 10
+        stackView.spacing = 20
         stackView.distribution = .fillEqually
         stackView.translatesAutoresizingMaskIntoConstraints = false
         
@@ -59,7 +90,7 @@ final class LoginViewController: UIViewController {
         stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 240).isActive = true
         stackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40).isActive = true
         stackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -40).isActive = true
-        stackView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        stackView.heightAnchor.constraint(equalToConstant: 300).isActive = true
     }
 
     @objc
@@ -75,11 +106,11 @@ final class LoginViewController: UIViewController {
         let isFormFilled = !emailText.isEmpty && !passwordText.isEmpty
 
         if isFormFilled {
-            signUpButton.backgroundColor = UIColor.orange
-            signUpButton.isEnabled = true
+            loginButton.backgroundColor = .white
+            loginButton.isEnabled = true
         } else {
-            signUpButton.backgroundColor = UIColor.lightGray
-            signUpButton.isEnabled = false
+            loginButton.backgroundColor = .lightGray
+            loginButton.isEnabled = false
         }
         
     }
