c17752eede3f8601c25344899c84b13803371183
perlinleo <perlinleo@github.com>
2022-04-10 22:18:26 +0300

    Feed card

---
+++ Drip/Components/CardViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CardViewController.swift
+//  Drip
+//
+//  Created by pierrelean on 05.04.2022.
+//
+
+import UIKit
+
+class CardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
