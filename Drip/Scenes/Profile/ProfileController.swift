88b785a6dd7f06129ceab3ddf6e2de043684c28b
Mikhail Popov <mikh.popov@vk.team>
2022-06-22 16:05:48 +0300

    added logout

--- Drip/Scenes/Profile/ProfileController.swift
+++ Drip/Scenes/Profile/ProfileController.swift
@@ -2,6 +2,15 @@ import UIKit
 import PinLayout
 
 final class ProfileViewController: UIViewController {
+    let factory = AppFactory()
+
+    let logoutBtn: UIButton = {
+            let button = UIButton(type: .custom)
+            button.setImage(UIImage(named: "settings"), for: .normal)
+            button.addTarget(self, action: #selector(touchLogoutBtn), for: .touchUpInside)
+            return button
+    }()
+
     let settingsBtn: UIButton = {
             let button = UIButton(type: .custom)
             button.setImage(UIImage(named: "settings"), for: .normal)
@@ -11,10 +20,27 @@ final class ProfileViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(logoutBtn)
         view.addSubview(settingsBtn)
-        settingsBtn.pin.right(view.pin.safeArea.right + 20).top(view.pin.safeArea.top + 30).height(50).width(50)
+        logoutBtn.pin.left(view.pin.safeArea.right + 20).top(view.pin.safeArea.top + 60).height(50).width(50)
+        settingsBtn.pin.right(view.pin.safeArea.right + 20).top(view.pin.safeArea.top + 60).height(50).width(50)
     }
     
+    @objc
+    func touchLogoutBtn() {
+        presentingViewController?.dismiss(animated: true, completion: nil)
+        let cookies = HTTPCookieStorage.shared.cookies(for: URL(string: "https://drip.monkeys.team/api/v1")!)
+        for cookie in cookies ?? [] {
+            HTTPCookieStorage.shared.deleteCookie(cookie as HTTPCookie)
+        }
+
+        let loginController = factory.buildLoginViewController()
+        let navController = UINavigationController(rootViewController: loginController)
+        loginController.modalTransitionStyle = .flipHorizontal
+        loginController.modalPresentationStyle = .fullScreen
+        present(navController, animated: true, completion: nil)
+    }
+
     @objc
     func touchSettingsBtn() {
         let profileController = ProfileSettingsViewController()
